6ebc91f6d03f35c8b8f883588bc2f61f602b59c9
CloudNiner <andrew.fink.11@gmail.com>
2015-05-07 16:14:38 -0400

    Add yo generated boilerplate

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mosGreenworksApp
+ * @description
+ * # mosGreenworksApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mosGreenworksApp', [
+    'ngCookies'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mosGreenworksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mosGreenworksApp
+ */
+angular.module('mosGreenworksApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
