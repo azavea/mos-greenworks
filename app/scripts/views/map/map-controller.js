856c95ac52d3523f743e14bc97fe288f5d0e25f5
CloudNiner <andrew.fink.11@gmail.com>
2015-05-15 17:53:23 -0400

    Update map on filter toggles changed
    Toggle individual subcategory
    Toggle project category
    Reset button
    Clear button
    
    FIXME: Empty array on clear is read as no-op

--- app/scripts/views/map/map-controller.js
+++ app/scripts/views/map/map-controller.js
@@ -27,13 +27,16 @@
 
             ctl.onProjectFilterClicked = onProjectFilterClicked;
             ctl.onSubFilterClicked = onSubFilterClicked;
+            ctl.onResetClicked = onResetClicked;
+            ctl.onClearClicked = onClearClicked;
 
             cartodb.createVis('map', 'https://greenworks.cartodb.com/api/v2/viz/33627780-f988-11e4-8ff7-0e0c41326911/viz.json')
                 .done(onVisReady, onVisError);
 
             Categories.get().then(function (categories) {
                 ctl.categories = categories;
-                ctl.filters =
+                ctl.toggles.project = allProjectKeys(ctl.categories);
+                ctl.toggles.sub = allKeysFromCategories(ctl.categories);
 
                 // Cheat and delay showing filters for one more digest cycle
                 // No matter what I can't seem to get the unstyled/open categories flash to
@@ -46,14 +49,18 @@
 
         function onProjectFilterClicked(key) {
             ctl.toggles.project[key] = !ctl.toggles.project[key];
-            $log.debug(key);
-            // TODO: Implement by project filtering logic
+            var val = ctl.toggles.project[key];
+
+            var subKeys = getKeysForCategory(ctl.categories, key);
+            angular.forEach(subKeys, function (subKey) {
+                ctl.toggles.sub[subKey] = val;
+            });
+            updateFilter();
         }
 
         function onSubFilterClicked(key) {
             ctl.toggles.sub[key] = !ctl.toggles.sub[key];
-            $log.debug(key);
-            // TODO: Implement key filtering logic
+            updateFilter();
         }
 
         function onVisReady(newVis) {
@@ -67,6 +74,96 @@
 
         function onVisError(error) {
             $log.error(error);
+        }
+
+        function updateFilter() {
+            var filterList = _(ctl.toggles.sub)
+                .pick(function (value) { return value; })
+                .keys()
+                .value();
+
+            sqlFilter.setWhere({ list: filterList });
+            if (cdbSubLayer) {
+                cdbSubLayer.setSQL(sqlFilter.makeSql());
+            }
+        }
+
+        function onResetClicked() {
+            setAllToggles(true);
+        }
+
+        function onClearClicked() {
+            setAllToggles(false);
+        }
+
+        function setAllToggles(value) {
+            angular.forEach(ctl.toggles.sub, function (v, key) {
+                ctl.toggles.sub[key] = value;
+            });
+            angular.forEach(ctl.toggles.project, function (v, key) {
+                ctl.toggles.project[key] = value;
+            });
+            updateFilter();
+        }
+
+        /**
+         * Helper to retrieve keys for a given product category
+         *
+         * Takes top level categories object
+         *
+         * @param  {[type]} categories [description]
+         * @return {[type]}            [description]
+         */
+        function getKeysForCategory(categories, searchKey) {
+            var keys = [];
+            angular.forEach(categories, function (value, key) {
+                if (searchKey === key) {
+                    keys = keys.concat(_.keys(value));
+                } else {
+                    keys = keys.concat(getKeysForCategory(value, searchKey));
+                }
+            });
+            return keys;
+        }
+
+        /**
+         * Helper to retrive all product_category keys (at the second level)
+         */
+        function allProjectKeys(categories) {
+            var keys = {};
+            angular.forEach(categories, function (value) {
+                var prodKeys = _.mapValues(value, function () {
+                    return true;
+                });
+                angular.extend(keys, prodKeys);
+            });
+            return keys;
+            /*
+            //play with this later
+            var data = _(categories).values()
+                .map(function(k) { return _.keys(k); })
+                .flatten()
+                .mapValues(function () { return true; })
+                .value();
+            $log.debug(data);
+            return data;
+            */
+        }
+
+        /**
+         * Helper function to create a single depth object of subcategory keys
+         * Defaults all keys to enabled
+         */
+        function allKeysFromCategories(categories) {
+            var keys = {};
+            angular.forEach(categories, function (value, key) {
+                if (value instanceof Object) {
+                    angular.extend(keys, allKeysFromCategories(value));
+                } else {
+                    keys[key] = true;
+                }
+            });
+            return keys;
         }
     }
 
