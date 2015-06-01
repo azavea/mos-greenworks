5deb16993192b5461e1f54b50b03b1850f09294b
CloudNiner <andrew.fink.11@gmail.com>
2015-06-01 13:06:41 -0400

    Refactor loadMap to take result

--- app/scripts/views/root/root-controller.js
+++ app/scripts/views/root/root-controller.js
@@ -20,22 +20,23 @@
 
         function search(searchText, magicKey) {
             Geocoder.search(searchText, magicKey)
-            .then(loadMap)
+            .then(function (results) {
+                if (results && results.length) {
+                    loadMap(results[0]);
+                }
+            })
             .catch(function (error) {
                 $log.debug('Error searching:', error);
             });
         }
 
-        function loadMap(results) {
-            if (results && results.length) {
-                var result = results[0];
-                $log.debug(result);
+        function loadMap(result) {
+            $log.debug(result);
 
-                var sw = L.latLng(result.extent.ymin, result.extent.xmin);
-                var ne = L.latLng(result.extent.ymax, result.extent.xmax);
-                var bounds = L.latLngBounds(sw, ne);
-                $state.go('map', { bbox: bounds.toBBoxString() });
-            }
+            var sw = L.latLng(result.extent.ymin, result.extent.xmin);
+            var ne = L.latLng(result.extent.ymax, result.extent.xmax);
+            var bounds = L.latLngBounds(sw, ne);
+            $state.go('map', { bbox: bounds.toBBoxString() }, { reload: true });
         }
     }
 
