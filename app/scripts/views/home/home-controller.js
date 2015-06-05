0c7bebab0792f690ca1f6f9016c0f8471c7f5dd0
CloudNiner <andrew.fink.11@gmail.com>
2015-06-05 12:14:34 -0400

    Closes #23: Search button submits typeahead
    Limit submit action to only results within the Philly bounds

--- app/scripts/views/home/home-controller.js
+++ app/scripts/views/home/home-controller.js
@@ -6,7 +6,16 @@
      * Controller for the gw app home view
      */
     /* ngInject */
-    function HomeController($log, $state, Geocoder, Sections) {
+    function HomeController($log, $state, Config, Geocoder, Sections) {
+
+        var searchBBox = [
+            Config.bounds.southWest.lng,
+            Config.bounds.southWest.lat,
+            Config.bounds.northEast.lng,
+            Config.bounds.northEast.lat
+        ].join(',');
+        var searchBounds = L.latLngBounds(Config.bounds.southWest, Config.bounds.northEast);
+
         var ctl = this;
 
         initialize();
@@ -16,22 +25,39 @@
             ctl.suggest = Geocoder.suggest;
             ctl.search = search;
 
+            ctl.onSearchButtonClicked = onSearchButtonClicked;
+
             ctl.sections = Sections;
         }
 
-        function search(searchText, magicKey) {
-            Geocoder.search(searchText, magicKey)
+        function search(searchText, magicKey, options) {
+            Geocoder.search(searchText, magicKey, options)
             .then(function (results) {
                 if (results && results.length) {
+
+                    $log.debug(results);
                     var result = results[0];
-                    var sw = L.latLng(result.extent.ymin, result.extent.xmin);
-                    var ne = L.latLng(result.extent.ymax, result.extent.xmax);
-                    var bounds = L.latLngBounds(sw, ne);
-                    $state.go('map', { bbox: bounds.toBBoxString() });
+                    var latLng = L.latLng(result.feature.geometry.y, result.feature.geometry.x);
+                    if (searchBounds.contains(latLng)) {
+                        var sw = L.latLng(result.extent.ymin, result.extent.xmin);
+                        var ne = L.latLng(result.extent.ymax, result.extent.xmax);
+                        var bounds = L.latLngBounds(sw, ne);
+                        $state.go('map', { bbox: bounds.toBBoxString() });
+                    } else {
+                        $log.debug('We could not find ' + searchText + ' near Philly');
+                    }
+                } else {
+                    $log.debug('No results near Philly for ' + searchText);
                 }
             })
             .catch(function (error) {
                 $log.debug('Error searching:', error);
+            });
+        }
+
+        function onSearchButtonClicked() {
+            search(ctl.searchText, null, {
+                bbox: searchBBox
             });
         }
     }
